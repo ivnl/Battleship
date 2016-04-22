150f753de7622556c50b17f8043f1f0d9dd7000f
Ivan L <ivansaur@gmail.com>
2016-04-22 02:41:42 -0400

    implement multiple ships

--- battleship.py
+++ battleship.py
@@ -1,81 +1,86 @@
 from random import randint
 from collections import defaultdict
 
-board = []
+# users display board
+displayboard = []
 
+# ship placement board
+answerboard = []
+
+# append all O's to display board
 for x in range(5):
-    board.append(["O"] * 5)
+    displayboard.append(["O"] * 5)
+    answerboard.append([False] * 5)
 
-
+# display board to user
 def print_board(board):
     for row in board:
         print(" ".join(row))
 
+# randomize a number for ship placement purpose
 def randomizer(board):
     return randint(0, len(board) - 1)
 
 
+print("Let's play Battleship!\n")
+print_board(displayboard)
 
+# let user input the amount of ships they would like placed on the board
+num_ships = int(input("\nHow many ships to be placed? "))
 
-print("Let's play Battleship!")
-print_board(board)
-
-num_ships = int(input("How many ships to be placed? #Greater than 0"))
+# dictionary of lists
 ships = defaultdict(list)
 
-def generate_ships(num_ships, board):
+# generate the number of ships the user specified and set their positions on the boards
+def generate_ships(num_ships, dis_board, ans_board):
 
     for i in range(num_ships):
-        ship_row = randomizer(board)
-        ship_col = randomizer(board)
+        ship_row = randomizer(dis_board)
+        ship_col = randomizer(dis_board)
+
+        while(ans_board[ship_row][ship_col]==True):
+            ship_row = randomizer(dis_board)
+            ship_col = randomizer(dis_board)
+
+        ans_board[ship_row][ship_col] = True
+
         ships[i].append(ship_row)
         ships[i].append(ship_col)
 
 
-
-    print(ships[0])
-    print(ships[1])
-    print(ships[2])
-generate_ships(num_ships, board)
+generate_ships(num_ships, displayboard, answerboard)
 
 
-# def random_row(board):
-#     return randint(0, len(board) - 1)
-#
-#
-# def random_col(board):
-#     return randint(0, len(board[0]) - 1)
-#
-# ship_row = random_row(board)
-# ship_col = random_col(board)
-
-
-print
 #print "Debug Row: {}".format(ship_row)
 #print "Debug Col: {}".format(ship_col)
-print
 
+
+ships_sunk = 0
 
 for turn in range(5):
-    guess_row = int(input("Guess Row:"))
-    guess_col = int(input("Guess Col:"))
-    all_destroyed = False
+    print("\nTurn", turn + 1)
+    guess_row = int(input("\nGuess Row: "))
+    guess_col = int(input("Guess Col: "))
 
-    if all_destroyed == True:
-        print("You sunk all the battleships! You win")
+    if ships_sunk == num_ships:
+        print("\nYou sunk all the battleships! You win")
+        break
 
-    if guess_row == ship_row and guess_col == ship_col:
-        print("Congratulations! You sunk a battleship!")
+    if answerboard[guess_row][guess_col] == True:
+        print("\nCongratulations! You sunk a battleship at loc: ",guess_row, guess_col)
+        displayboard[guess_row][guess_col] = "!"
+
+        ships_sunk+=1
+        print(num_ships-ships_sunk, "ships remaining")
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print("Oops, that's not even in the ocean.")
-        elif board[guess_row][guess_col] == "X":
-            print("You guessed that one already.")
+            print("\nOops, that's not even in the ocean.")
+        elif displayboard[guess_row][guess_col] == "X":
+            print("\nYou guessed that one already.")
         else:
-            print("You missed my battleship!")
-            board[guess_row][guess_col] = "X"
-        print ("Turn", turn + 1)
-        print_board(board)
-    if turn == 3:
+            print("\nYou didn't hit anything!")
+            displayboard[guess_row][guess_col] = "X"
+        print_board(displayboard)
+    if turn == 4:
         print
-        print("Game Over")
+        print("\nOut of turns, Game Over")
