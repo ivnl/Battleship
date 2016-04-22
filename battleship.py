db96d95a7cc112ab0a2194d6dcff71edb5f00274
Ivan L <ivansaur@gmail.com>
2016-04-22 11:52:41 -0400

    implement multiple rounds

--- battleship.py
+++ battleship.py
@@ -7,10 +7,22 @@ displayboard = []
 # ship placement board
 answerboard = []
 
-# append all O's to display board
-for x in range(5):
-    displayboard.append(["O"] * 5)
-    answerboard.append([False] * 5)
+# dictionary of lists to store ship properties
+ships = defaultdict(list)
+
+# append all O's to display board and set all to false
+def initialize_board(dis_board, ans_board):
+    for x in range(5):
+        dis_board.append(["O"] * 5)
+        ans_board.append([False] * 5)
+
+# reset board values for new game
+def wipe_board(dis_board, ans_board):
+    ships_sunk = 0
+    for y in range(5):
+        for x in range(5):
+            displayboard[y][x] = "O"
+            answerboard[y][x] = False
 
 # display board to user
 def print_board(board):
@@ -20,16 +32,6 @@ def print_board(board):
 # randomize a number for ship placement purpose
 def randomizer(board):
     return randint(0, len(board) - 1)
-
-
-print("Let's play Battleship!\n")
-print_board(displayboard)
-
-# let user input the amount of ships they would like placed on the board
-num_ships = int(input("\nHow many ships to be placed? "))
-
-# dictionary of lists
-ships = defaultdict(list)
 
 # generate the number of ships the user specified and set their positions on the boards
 def generate_ships(num_ships, dis_board, ans_board):
@@ -48,39 +50,57 @@ def generate_ships(num_ships, dis_board, ans_board):
         ships[i].append(ship_col)
 
 
-generate_ships(num_ships, displayboard, answerboard)
+initialize_board(displayboard, answerboard)
+play_again = "yes"
 
+while play_again == "yes":
 
-#print "Debug Row: {}".format(ship_row)
-#print "Debug Col: {}".format(ship_col)
+    ships_sunk = 0
+    wipe_board(displayboard, answerboard)
 
+    print("Let's play Battleship!\n")
+    print_board(displayboard)
 
-ships_sunk = 0
-
-for turn in range(5):
-    print("\nTurn", turn + 1)
-    guess_row = int(input("\nGuess Row: "))
-    guess_col = int(input("Guess Col: "))
-
-    if ships_sunk == num_ships:
-        print("\nYou sunk all the battleships! You win")
+    # let user input the amount of ships they would like placed on the board
+    num_ships = int(input("\nHow many ships to be placed? "))
+    if num_ships>25: ###needs improvement
+        print("Number of ships can't exceed size of the grid!")
         break
 
-    if answerboard[guess_row][guess_col] == True:
-        print("\nCongratulations! You sunk a battleship at loc: ",guess_row, guess_col)
-        displayboard[guess_row][guess_col] = "!"
+    generate_ships(num_ships, displayboard, answerboard)
 
-        ships_sunk+=1
-        print(num_ships-ships_sunk, "ships remaining")
-    else:
-        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-            print("\nOops, that's not even in the ocean.")
-        elif displayboard[guess_row][guess_col] == "X":
-            print("\nYou guessed that one already.")
+    #print "Debug Row: {}".format(ship_row)
+    #print "Debug Col: {}".format(ship_col)
+
+    for turn in range(5):
+
+        if ships_sunk == num_ships:
+            print("\nYou sunk all the battleships! You win")
+            break
+
+        print("\nTurn", turn + 1)
+        guess_row = int(input("\nGuess Row: "))
+        guess_col = int(input("Guess Col: "))
+
+        if answerboard[guess_row][guess_col] == True:
+            print("\nCongratulations! You sunk a battleship at loc: ",guess_row, guess_col)
+            displayboard[guess_row][guess_col] = "!"
+
+            ships_sunk+=1
+            print(num_ships-ships_sunk, "ships remaining")
         else:
-            print("\nYou didn't hit anything!")
-            displayboard[guess_row][guess_col] = "X"
+            if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+                print("\nOops, that's not even in the ocean.")
+            elif displayboard[guess_row][guess_col] == "X":
+                print("\nYou guessed that one already.")
+            else:
+                print("\nYou didn't hit anything!")
+                displayboard[guess_row][guess_col] = "X"
+
         print_board(displayboard)
-    if turn == 4:
-        print
-        print("\nOut of turns, Game Over")
+
+        if turn == 4:
+            print
+            print("\nOut of turns, Game Over")
+
+    play_again = input("Play again? (yes/no): ")
